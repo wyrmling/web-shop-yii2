fc0a9bcace25ab84baf78d12fa736b0038c5ef60
Shilling <shilling@mail.ru>
2015-10-21 00:55:04 +0300

    add EntryForm and views for it

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -8,6 +8,7 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\EntryForm;
 
 class SiteController extends Controller
 {
@@ -97,4 +98,20 @@ class SiteController extends Controller
         return $this->render('say', ['message' => $message]);
     }
 
+    public function actionEntry()
+    {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // данные в $model удачно проверены
+
+            // делаем что-то полезное с $model ...
+ 
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // либо страница отображается первый раз, либо есть ошибка в данных
+            return $this->render('entry', ['model' => $model]);
+        }
+    }
+    
 }
---
+++ views/site/entry-confirm.php
@@ -0,0 +1,9 @@
+<?php
+use yii\helpers\Html;
+?>
+<p>Вы ввели следующую информацию:</p>
+
+<ul>
+    <li><label>Name</label>: <?= Html::encode($model->name) ?></li>
+    <li><label>Email</label>: <?= Html::encode($model->email) ?></li>
+</ul>
