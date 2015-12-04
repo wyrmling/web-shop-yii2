d68480f2763a7b604dde1e0cd91d16a6345eede5
Green Drake <drake@green.com>
2015-12-05 00:57:28 +0200

    rbac roles

--- migrations/m151204_225806_rbac_init.php
+++ migrations/m151204_225806_rbac_init.php
@@ -8,6 +8,7 @@
 use yii\base\InvalidConfigException;
 use yii\db\Schema;
 use yii\rbac\DbManager;
+use app\rbac\UserGroupRule;
 
 /**
  * Initializes RBAC tables
@@ -77,7 +78,11 @@ class m151204_225806_rbac_init extends \yii\db\Migration
             'PRIMARY KEY (item_name, user_id)',
             'FOREIGN KEY (item_name) REFERENCES ' . $authManager->itemTable . ' (name) ON DELETE CASCADE ON UPDATE CASCADE',
         ], $tableOptions);
+
+        $this->initRbac();
     }
+
+
 
     public function down()
     {
@@ -89,4 +94,75 @@ class m151204_225806_rbac_init extends \yii\db\Migration
         $this->dropTable($authManager->itemTable);
         $this->dropTable($authManager->ruleTable);
     }
+
+    protected function initRbac()
+    {
+        $authManager = $this->getAuthManager();
+
+        // Create roles
+        $guest  = $authManager->createRole('guest');
+        $brand  = $authManager->createRole('BRAND');
+        $talent = $authManager->createRole('TALENT');
+        $admin  = $authManager->createRole('admin');
+
+        // Create simple, based on action{$NAME} permissions
+        $login  = $authManager->createPermission('login');
+        $logout = $authManager->createPermission('logout');
+        $error  = $authManager->createPermission('error');
+        $signUp = $authManager->createPermission('sign-up');
+        $index  = $authManager->createPermission('index');
+        $view   = $authManager->createPermission('view');
+        $update = $authManager->createPermission('update');
+        $delete = $authManager->createPermission('delete');
+
+        // Add permissions in Yii::$app->authManager
+        $authManager->add($login);
+        $authManager->add($logout);
+        $authManager->add($error);
+        $authManager->add($signUp);
+        $authManager->add($index);
+        $authManager->add($view);
+        $authManager->add($update);
+        $authManager->add($delete);
+
+
+        // Add rule, based on UserExt->group === $user->group
+        $userGroupRule = new UserGroupRule();
+        $authManager->add($userGroupRule);
+
+        // Add rule "UserGroupRule" in roles
+        $guest->ruleName  = $userGroupRule->name;
+        $brand->ruleName  = $userGroupRule->name;
+        $talent->ruleName = $userGroupRule->name;
+        $admin->ruleName  = $userGroupRule->name;
+
+        // Add roles in Yii::$app->authManager
+        $authManager->add($guest);
+        $authManager->add($brand);
+        $authManager->add($talent);
+        $authManager->add($admin);
+
+        // Add permission-per-role in Yii::$app->authManager
+        // Guest
+        $authManager->addChild($guest, $login);
+        $authManager->addChild($guest, $logout);
+        $authManager->addChild($guest, $error);
+        $authManager->addChild($guest, $signUp);
+        $authManager->addChild($guest, $index);
+        $authManager->addChild($guest, $view);
+
+        // BRAND
+        $authManager->addChild($brand, $update);
+        $authManager->addChild($brand, $guest);
+
+        // TALENT
+        $authManager->addChild($talent, $update);
+        $authManager->addChild($talent, $guest);
+
+        // Admin
+        $authManager->addChild($admin, $delete);
+        $authManager->addChild($admin, $talent);
+        $authManager->addChild($admin, $brand);
+    }
+
 }
