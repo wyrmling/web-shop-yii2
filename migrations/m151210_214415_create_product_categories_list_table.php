d344907456c86510de1869eef9d5b3f0e5ebdbac
Green Drake <drake@green.com>
2016-01-04 00:10:13 +0200

    test content fixed

--- migrations/m151210_214415_create_product_categories_list_table.php
+++ migrations/m151210_214415_create_product_categories_list_table.php
@@ -17,78 +17,81 @@ class m151210_214415_create_product_categories_list_table extends Migration
             'parent_category_id' => $this->integer()->notNull(),
             'name' => $this->string()->notNull(),
         ]);
-        
+
         $this->insert('{{product_categories_list}}', [
             'category_id' => 1,
             'parent_category_id' => 0,
-            'name' => 'category_1',
+            'name' => '1_Комплектующие',
         ]);
-        
-        $this->insert('{{product_categories_list}}', [
-            'category_id' => 2,
-            'parent_category_id' => 0,
-            'name' => 'category_2',
-        ]);
-        
-        $this->insert('{{product_categories_list}}', [
-            'category_id' => 3,
-            'parent_category_id' => 0,
-            'name' => 'category_3',
-        ]);
-        
-        $this->insert('{{product_categories_list}}', [
-            'category_id' => 4,
-            'parent_category_id' => 0,
-            'name' => 'category_4',
-        ]);
-        
         $this->insert('{{product_categories_list}}', [
             'category_id' => 5,
             'parent_category_id' => 1,
-            'name' => 'category_1_1',
+            'name' => '1_1_Процессоры',
         ]);
-        
         $this->insert('{{product_categories_list}}', [
             'category_id' => 6,
             'parent_category_id' => 5,
-            'name' => 'category_1_1_1',
+            'name' => '1_1_1_AMD',
         ]);
-        
+        $this->insert('{{product_categories_list}}', [
+            'category_id' => 14,
+            'parent_category_id' => 5,
+            'name' => '1_1_2_Intel',
+        ]);
         $this->insert('{{product_categories_list}}', [
             'category_id' => 7,
             'parent_category_id' => 1,
-            'name' => 'category_1_2',
+            'name' => '1_2_Видеокарты',
         ]);
-        
+        $this->insert('{{product_categories_list}}', [
+            'category_id' => 13,
+            'parent_category_id' => 7,
+            'name' => '1_2_1_AMD',
+        ]);
         $this->insert('{{product_categories_list}}', [
             'category_id' => 8,
             'parent_category_id' => 7,
-            'name' => 'category_1_2_1',
+            'name' => '1_2_2_Nvidia',
         ]);
-        
+
+        $this->insert('{{product_categories_list}}', [
+            'category_id' => 2,
+            'parent_category_id' => 0,
+            'name' => '2_Бытовая техника',
+        ]);
         $this->insert('{{product_categories_list}}', [
             'category_id' => 9,
             'parent_category_id' => 2,
-            'name' => 'category_2_1',
+            'name' => '2_1_Пылесосы',
         ]);
-        
         $this->insert('{{product_categories_list}}', [
             'category_id' => 10,
             'parent_category_id' => 2,
-            'name' => 'category_2_2',
+            'name' => '2_2_Чайники',
         ]);
-        
+
+        $this->insert('{{product_categories_list}}', [
+            'category_id' => 3,
+            'parent_category_id' => 0,
+            'name' => '3_Телефоны',
+        ]);
         $this->insert('{{product_categories_list}}', [
             'category_id' => 11,
             'parent_category_id' => 3,
-            'name' => 'category_3_1',
+            'name' => '3_1_Смартфоны',
         ]);
-        
         $this->insert('{{product_categories_list}}', [
             'category_id' => 12,
             'parent_category_id' => 11,
-            'name' => 'category_3_1_1',
+            'name' => '3_1_1_Android',
         ]);
+
+        $this->insert('{{product_categories_list}}', [
+            'category_id' => 4,
+            'parent_category_id' => 0,
+            'name' => '4_Ёлочные игрушки',
+        ]);
+
     }
 
     public function down()
