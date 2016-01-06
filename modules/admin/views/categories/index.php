5f2f9dab8e10c60f38d7ba5087cf844b03bb6c43
Green Drake <drake@green.com>
2016-01-06 23:05:42 +0200

    small fixes

--- modules/admin/views/categories/index.php
+++ modules/admin/views/categories/index.php
@@ -1,10 +1,11 @@
 <?php
 
-use yii\helpers\Url;
+use yii\helpers\Html;
 
 $this->params['breadcrumbs'][] = ['label' => 'Admin', 'url' => ['/admin']];
 $this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
 
+echo Html::a('Добавить категорию', ['/admin/categories/add/', 'id' => 0], ['class'=>'btn btn-primary']);
 echo buildTree($tree, 0);
 
 function buildTree($cats, $parent_id)
@@ -13,9 +14,9 @@ function buildTree($cats, $parent_id)
         $tree = '<ul>';
         foreach ($cats[$parent_id] as $cat) {
             $tree .= '<li>' . $cat['category_id'] . ' - ' . $cat['name']
-                    . '<a href=' . Url::to(['/admin/categories/edit/', 'id' => $cat['category_id']]) . '> [переименовать]</a>'
-                    . '<a href=' . Url::to(['/admin/categories/delete/', 'id' => $cat['category_id']]) . '> [удалить]</a>'
-                    . '<a href=' . Url::to(['/admin/categories/add/', 'id' => $cat['category_id']]) . '> [добавить подкатегорию]</a>';
+                    . Html::a('[переименовать]', ['categories/edit', 'id' => $cat['category_id']])
+                    . Html::a('[удалить]', ['categories/delete', 'id' => $cat['category_id']])
+                    . Html::a('[добавить подкатегорию]', ['categories/add', 'id' => $cat['category_id']]);
             $tree .= buildTree($cats, $cat['category_id']);
             $tree .= '</li>';
         }
@@ -23,5 +24,3 @@ function buildTree($cats, $parent_id)
         return $tree;
     }
 }
-
-echo '<a href=' . Url::to(['/admin/categories/add/', 'id' => 0]) . '> [добавить категорию]</a>';
--- views/articles/_articles_list.php
+++ views/articles/_articles_list.php
@@ -6,8 +6,8 @@ use yii\helpers\Html;
 
 ?>
 <div class="post">
-    <a href="<?= Url::to(['articles/read/', 'id' => "$model->article_id"]); ?>"><h2><?= Html::encode($model->title) ?></h2></a>
-    <?= HtmlPurifier::process($model->description) ?>  
+    <?= Html::a('<h2>'. Html::encode($model->title) .'</h2>', ['articles/read', 'id' => $model->article_id]) ?>
+    <?= HtmlPurifier::process($model->description) ?>
     <br>
     <?= Html::encode($model->time_created) ?>
     <br>
