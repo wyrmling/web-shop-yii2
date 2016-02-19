393e5f354733d4e70ed390a5542d99cf5a6e2126
Shilling <shilling@mail.ru>
2016-02-20 01:40:07 +0200

    little fix for cart

--- views/cart/index.php
+++ views/cart/index.php
@@ -1,23 +1,46 @@
 <?php
 
 use yii\helpers\Html;
+
+$summ = 0;
 ?>
 
 <h2>Корзина</h2>
 
-<?php if(!count(Yii::$app->session->get('productsarray'))){
-    echo 'Нет выбранных товаров';
-}
-?>
-
-<?php foreach (Yii::$app->session->get('productsarray') as $key => $value): ?>
+<?php if (!count(Yii::$app->session->get('productsarray'))): ?>
 
     <div>
-        <?= Html::encode($products[$value]['title']) ?>
-        <?= Html::encode($products[$value]['brand_name']) ?>
-        <?= Html::encode($products[$value]['price']) ?>
-        ( <?= Html::encode($products[$value]['special_price']) ?> )
-        <?= Html::a('[удалить из корзины]', ['/cart/delete', 'id' => $key]) ?>
+        Нет выбранных товаров
     </div>
 
-<?php endforeach; ?>
+<?php else: ?>
+
+    <?php foreach (Yii::$app->session->get('productsarray') as $key => $value): ?>
+
+        <div>
+            <b> <?= Html::encode($products[$value]['title']) ?> </b>
+            <?= Html::encode($products[$value]['brand_name']) ?>
+            <?= Html::encode($products[$value]['price']) ?>
+            ( <?= Html::encode($products[$value]['special_price']) ?> )
+            <?= Html::a('[удалить из корзины]', ['/cart/delete', 'id' => $key]) ?>
+        </div>
+
+        <?php
+        if (isset($products[$value]['special_price'])) {
+            $summ += $products[$value]['special_price'];
+        } else {
+            $summ += $products[$value]['price'];
+        }
+        ?>
+
+    <?php endforeach; ?>
+<?php endif; ?>
+
+<br><br>
+<div>
+    Общая сумма заказа: <?= $summ ?>
+</div>
+<br><br>
+<div>
+    <?= Html::a('Сделать заказ', ['/cart/order',], ['class' => 'btn btn-success']); ?>
+</div>
