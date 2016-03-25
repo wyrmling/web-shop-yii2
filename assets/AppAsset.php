be816cf5d85e48479c505add581daa94a3fe47f7
shilling <shill@mail.ru>
2016-03-25 16:40:00 +0200

    done dynamic tree for Categories

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -19,8 +19,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/categorytree.css',
     ];
     public $js = [
+        'js/categorytree.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- modules/admin/views/categories/index.php
+++ modules/admin/views/categories/index.php
@@ -1,29 +1,44 @@
 <?php
 
 use yii\helpers\Html;
-use app\models\Categories;
 
 $this->params['breadcrumbs'][] = ['label' => 'Администрирование', 'url' => ['/admin']];
 $this->params['breadcrumbs'][] = ['label' => 'Категории', 'url' => ['index']];
 
-echo Html::a('Добавить категорию', ['/admin/categories/add/', 'id' => 0], ['class' => 'btn btn-success']);
 
-echo buildTree(0, $tree, $quantities);
+?>
+<div class="tree">
+    
+    <?php
+    echo Html::a('Добавить категорию', ['/admin/categories/add/', 'id' => 0], ['class' => 'btn-category-add']);
+    
+    echo buildTree(0, $tree, $quantities);
 
-function buildTree($start, $cats, $quant)
-{
-    if (isset($cats[$start])) {
-        $tree = '<ul>';
-        foreach ($cats[$start] as $cat_id => $name) {
-            $tree .= '<li>' . $cat_id . ' - ' . $name . ' (' . $quant[$cat_id]['quantity_visible'] . ') ' . ' (' . $quant[$cat_id]['quantity_invisible'] . ') '
-                    . Html::a('[переименовать]', ['categories/edit', 'id' => $cat_id])
-                    . Html::a('[удалить]', ['categories/delete', 'id' => $cat_id])
-                    . Html::a('[добавить подкатегорию]', ['categories/add', 'id' => $cat_id])
-                    . Html::a('[список атрибутов]', ['attributes/list', 'id' => $cat_id]);
-            $tree .= buildTree($cat_id, $cats, $quant);
-            $tree .= '</li>';
+    function buildTree($start, $cats, $quant)
+    {
+        if (isset($cats[$start])) {
+            $tree = '<ul>';
+            foreach ($cats[$start] as $cat_id => $name) {
+                if ($quant[$cat_id]['parent_category_id'] == 0) {
+                    $stile_class = '<span><i class="icon-folder-open"></i>';
+                } elseif (isset($cats[$cat_id])) {
+                    $stile_class = '<span><i class="icon-minus-sign"></i>';
+                } else {
+                    $stile_class = '<span><i class="icon-leaf"></i>';
+                }
+                $tree .= '<li>' . $stile_class . $cat_id . ' - ' . $name . ' (' . $quant[$cat_id]['quantity_visible'] . ') ' . ' (' . $quant[$cat_id]['quantity_invisible'] . ') '
+                        
+                        . Html::a('переименовать', ['categories/edit', 'id' => $cat_id], ['class' => 'btn-category-edit'])
+                        . Html::a('удалить', ['categories/delete', 'id' => $cat_id], ['class' => 'btn-category-delete'])
+                        . Html::a('добавить подкатегорию', ['categories/add', 'id' => $cat_id], ['class' => 'btn-category-add'])
+                        . Html::a('список атрибутов', ['attributes/list', 'id' => $cat_id], ['class' => 'btn-category-list'])
+                . '</span>';
+                $tree .= buildTree($cat_id, $cats, $quant);
+                $tree .= '</li>';
+            }
+            $tree .= '</ul>';
+            return $tree;
         }
-        $tree .= '</ul>';
-        return $tree;
     }
-}
+    ?>
+</div>
