11f6edb8a64672244843e0786bc429c0794e2436
shilling <Shilling2ndpara@users.noreply.github.com>
2016-06-14 16:06:28 +0300

    tried dounload images to temp folder - all working

--- models/Images.php
+++ models/Images.php
@@ -4,6 +4,8 @@ namespace app\models;
 
 use Yii;
 use yii\db\ActiveRecord;
+use yii\base\Model;
+use yii\web\UploadedFile;
 
 /**
  * This is the model class for table "images".
@@ -15,6 +17,7 @@ use yii\db\ActiveRecord;
  */
 class Images extends ActiveRecord
 {
+
     const OBJECT_TYPE_FOR_PRODUCTS = 1;
     const OBJECT_TYPE_FOR_ARTICLES = 2;
     const OBJECT_TYPE_FOR_NEWS = 3;
@@ -22,6 +25,8 @@ class Images extends ActiveRecord
     const OBJECT_TYPE_FOR_USERS = 5;
     const OBJECT_TYPE_FOR_COMMERCIAL = 6;
     const OBJECT_TYPE_FOR_BRANDS = 7;
+
+    public $imageFile;
 
     public static function tableName()
     {
@@ -31,9 +36,10 @@ class Images extends ActiveRecord
     public function rules()
     {
         return [
-            [['object_type_id', 'object_id'], 'required'],
-            [['object_type_id', 'object_id'], 'integer'],
-            [['image_title'], 'string', 'max' => 255],
+    //        [['object_type_id', 'object_id'], 'required'],
+    //        [['object_type_id', 'object_id'], 'integer'],
+    //       [['image_title'], 'string', 'max' => 255],
+            [['imageFile'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg'],
         ];
     }
 
@@ -45,6 +51,20 @@ class Images extends ActiveRecord
             'object_id' => 'ID объекта',
             'image_title' => 'название картинки',
         ];
+    }
+
+    public function upload()
+    {
+        if ($this->validate()) {
+            $structure = dirname(__DIR__) . '/uploads/487/asd/7/';
+            if (!is_dir($structure)){
+            mkdir($structure, 0777, true);
+            }
+            $this->imageFile->saveAs($structure . $this->imageFile->baseName . '.' . $this->imageFile->extension);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- modules/admin/controllers/ImagesController.php
+++ modules/admin/controllers/ImagesController.php
@@ -5,11 +5,24 @@ namespace app\modules\admin\controllers;
 use Yii;
 use app\components\Controller;
 use app\models\Images;
+use yii\web\UploadedFile;
 
 class ImagesController extends Controller
 {
-//    public function actionIndex()
-//    {
-//        return $this->render('index');
-//    }
+
+    public function actionIndex()
+    {
+        $model = new Images();
+
+        if (Yii::$app->request->isPost) {
+            $model->imageFile = UploadedFile::getInstance($model, 'imageFile');
+            if ($model->upload()) {
+                // file is uploaded successfully
+                return;
+            }
+        }
+
+        return $this->render('index', ['model' => $model]);
+    }
+
 }
